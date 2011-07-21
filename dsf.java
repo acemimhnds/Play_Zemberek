ad05b4407ea5f1749b67d6f1c07443923325b3ca
Merve ÇAVDAR <mhndsmerve@hotmail.com>
2011-07-21 10:50:00 +0300

    eksik dosyalar eklendi

---
+++ dsf.java
@@ -0,0 +1,20 @@
+package models;
+
+import play.*;
+import play.db.jpa.*;
+
+import javax.persistence.*;
+import java.util.*;
+
+@Entity
+public class dsf extends Model {
+    String kok;
+    
+    public void dsf(String val){
+        this.kok=val;        
+        
+    }
+    
+    
+    
+}
